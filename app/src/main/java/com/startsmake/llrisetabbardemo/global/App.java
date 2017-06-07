213388e6fc02af1937e06bf0c4144dd75ca69137
taodaren <taodaren1125@gmail.com>
2017-06-07 20:16:51 +0800

    so文件bug待解决

--- app/src/main/java/com/startsmake/llrisetabbardemo/global/App.java
+++ app/src/main/java/com/startsmake/llrisetabbardemo/global/App.java
@@ -1,24 +1,22 @@
 package com.startsmake.llrisetabbardemo.global;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.yixia.camera.VCamera;
 
 import java.io.File;
 
-/**
- * Created by zhaoshuang on 17/2/8.
- */
-
 public class App extends Application {
-    public static String VIDEO_PATH =  "/sdcard/video_cache/";
+    public static String VIDEO_PATH = "/sdcard/video_cache/";
+
     @Override
     public void onCreate() {
         super.onCreate();
 
         VIDEO_PATH += String.valueOf(System.currentTimeMillis());
         File file = new File(VIDEO_PATH);
-        if(!file.exists()) file.mkdirs();
+        if (!file.exists()) file.mkdirs();
 
         //设置视频缓存路径
         VCamera.setVideoCachePath(VIDEO_PATH);
@@ -28,5 +26,12 @@ public class App extends Application {
 
         // 初始化拍摄SDK，必须
         VCamera.initialize(this);
+
+        File dir = getApplicationContext().getDir("l  ibs", Context.MODE_PRIVATE);
+        File[] currentFiles;
+        currentFiles = dir.listFiles();
+        for (int i = 0; i < currentFiles.length; i++) {
+            System.load(currentFiles[i].getAbsolutePath());
+        }
     }
 }
