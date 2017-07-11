6c70d265186df8140a091993fb307e3db45b4d04
taodaren <taodaren1125@gmail.com>
2017-07-11 23:21:21 +0800

    更新 WebView URL

--- app/src/main/java/com/startsmake/llrisetabbardemo/global/internet/WebUrl.java
+++ app/src/main/java/com/startsmake/llrisetabbardemo/global/internet/WebUrl.java
@@ -6,21 +6,47 @@ package com.startsmake.llrisetabbardemo.global.internet;
 
 public class WebUrl {
     //全局通用
-    public static final String ALL_URL = "http://im.zxb01.com/index";
+    public static final String BASE_URL = "http://im.zxb01.com/index";
 
     //忘记密码
-    public static final String FORGET_PASSWORD = ALL_URL + "/pub/forgetpwd.html";
+    public static final String FORGET_PASSWORD = BASE_URL + "/pub/forgetpwd.html";
 
     //首页
     public static final String TAB_HOME = "http://192.168.1.145/two/pages/index.html";
+
+
     //社区
-    public static final String TAB_WORD = "http://192.168.1.145/two/pages/world.html";
+    public static final String TAB_WORD = BASE_URL + "community/index.html";
+    //社区管理
+    public static final String admin = "community/admin.html";
+    public static final String apply = "community/apply.html";
+    public static final String apply_ = "community/apply_.html";
+    public static final String apply_admin = "community/apply_admin.html";
+    //专区（更多）
+    public static final String area = "community/area.html";
+    //专栏（更多）
+    public static final String column = "community/column.html";
+    //专题（更多）
+    public static final String special = "community/special.html";
+    //社区详情
+    public static final String Community = "community/Community.html";
+    public static final String DATA = "community/data.html";
+    //帖子详情
+    public static final String details = "community/details.html";
+    public static final String forum_data = "community/forum_data.html";
+    //社区成员
+    public static final String forum_member = "community/forum_member.html";
+    public static final String moreforum = "community/moreforum.html";
+    //发帖
+    public static final String send = "community/send.html";
+    public static final String send_select = "community/send_select.html";
+
 
 
     //社团主界面
-    public static final String TAB_LEAGUE = ALL_URL + "/club/activity.html";
+    public static final String TAB_LEAGUE = BASE_URL + "/club/activity.html";
     //更多活动
-    public static final String MORE_ACTIVITY = ALL_URL + "/club/activity.html";
+    public static final String MORE_ACTIVITY = BASE_URL + "/club/activity.html";
     //更多作品
-    public static final String MORE_WORKS = ALL_URL + "/club/works.html";
+    public static final String MORE_WORKS = BASE_URL + "/club/works.html";
 }
