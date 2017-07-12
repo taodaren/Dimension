4c4f7fa97a7a10496e493ff37ef8e3b162e301dc
taodaren <taodaren1125@gmail.com>
2017-07-12 15:19:54 +0800

    有盟工程配置

---
+++ app/src/main/java/com/startsmake/llrisetabbardemo/WBShareActivity.java
@@ -0,0 +1,12 @@
+package com.startsmake.llrisetabbardemo;
+
+import com.umeng.socialize.media.WBShareCallBackActivity;
+
+
+/**
+ * 新浪微博回调 Activity
+ */
+
+public class WBShareActivity extends WBShareCallBackActivity {
+
+}
---
+++ app/src/main/java/com/startsmake/llrisetabbardemo/wxapi/WXEntryActivity.java
@@ -0,0 +1,17 @@
+package com.startsmake.llrisetabbardemo.wxapi;
+
+
+import com.umeng.weixin.callback.WXCallbackActivity;
+
+//import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+
+/**
+ * 微信回调 Activity
+ */
+
+public class WXEntryActivity extends WXCallbackActivity {
+
+
+
+}
